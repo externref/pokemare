48fa863bdf26d77142121b1e473710e6caac3c9b
sarthak-py <shiva02939@gmail.com>
2022-02-15 13:07:01 +0530

    removed unused imports

--- pokemare/cogs/general.py
+++ pokemare/cogs/general.py
@@ -1,14 +1,15 @@
 from disnake.embeds import Embed
-from disnake.ext.commands.bot import Bot
 from disnake.ext.commands.cog import Cog
 from disnake.ext.commands.core import command
 from disnake.ext.commands.context import Context
+
+from .. import PokeMare
 
 
 class General(Cog, name="General Commands"):
     """Some usual bot commands"""
 
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: PokeMare):
         self.hidden = False
         self.emoji = "😁"
         self.bot = bot
@@ -37,22 +38,22 @@ Pokémare is not affiliated with Nintendo, Creatures Inc. and GAME FREAK Inc.
         )  # .set_image(url='https://media.discordapp.net/attachments/936820715938795561/937542712473812992/image0.gif')
         await ctx.reply(embed=embed)
 
-    @command(name="ping", description="Latency of the Bot")
+    @command(name="ping", description="Latency of the PokeMare")
     async def _ping(self, ctx: Context):
         """Pong"""
         await ctx.send(f"🏓 `{round(ctx.bot.latency*1000 , 2)}ms` Pong ! ")
 
     @command(name="invite", description="Invite the bot to your servers!")
     async def _invite(self, ctx: Context):
-        """Bot invite"""
+        """PokeMare invite"""
         await ctx.send(
             f"> The bot is still in a development stage! You would like to test it out so we have made the invite link available",
             embed=Embed(
                 color=ctx.bot.color,
-                description=f"> {ctx.bot.get_emoji(866957054278631444)} Invite Bot by [`clicking here`]({ctx.bot.invite_url})\n> ♥️ We have a [`support server`]({ctx.bot.support_server_invite_url}) too",
+                description=f"> {ctx.bot.get_emoji(866957054278631444)} Invite PokeMare by [`clicking here`]({ctx.bot.invite_url})\n> ♥️ We have a [`support server`]({ctx.bot.support_server_invite_url}) too",
             ),
         )
 
 
-def setup(bot: Bot):
+def setup(bot: PokeMare):
     bot.add_cog(General(bot))
