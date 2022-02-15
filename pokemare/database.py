1662c7c241dccfa1aa60527b8dbec18c48dab53b
sarthak-py <shiva02939@gmail.com>
2022-02-15 13:06:15 +0530

    database class

---
+++ pokemare/database.py
@@ -0,0 +1,53 @@
+import aiosqlite
+from typing import Optional, Tuple
+from disnake.ext.commands.bot import Bot
+
+
+class UserInfoDatabase:
+    def __init__(self, database_file_path, bot: Bot):
+        self.databse_file = database_file_path
+        self.bot = bot
+
+    async def create_and_connect(self):
+        self.user_database: aiosqlite.Connection = await aiosqlite.connect(
+            self.databse_file
+        )
+        await self.user_database.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users
+            (
+                user_id TEXT,
+                gender TEXT,
+                starter_id TEXT,
+                badges TEXT,
+                pokedollars TEXT,
+                stars TEXT
+            )
+            """
+        )
+        await self.user_database.commit()
+
+    async def get_user_information(self, user_id) -> Optional[Tuple]:
+        async with self.user_database.cursor() as cursor:
+            await cursor.execute(
+                """
+                SELECT * FROM users
+                WHERE user_id = ?
+                """,
+                (user_id,),
+            )
+            return await cursor.fetchone()
+
+    async def insert_user_into_database(
+        self, user_id: str, gender: str, starter_id: str
+    ) -> None:
+        async with self.user_database.cursor() as cursor:
+            await cursor.execute(
+                """
+                INSERT INTO users
+                ( user_id , gender , starter_id ,  pokedollars , stars )
+                VALUES ( ? , ? , ? , ? , ?)
+                """,
+                (user_id, gender, starter_id, "2500", "0"),
+            )
+            await self.user_database.commit()
