2aaccd8b2261aba5a12aecee713ee56b5ae47ab5
sarthak-py <shiva02939@gmail.com>
2022-03-09 16:09:41 +0530

    removed old files

--- pokemare/user.py
+++
@@ -1,8 +0,0 @@
-class User(object):
-    def __init__(self, identifier, name):
-        self.identifier = identifier
-        self.name = name
-
-    def __copy__(self):
-        user_copy = type(self)(self.identifier, self.name)
-        return user_copy
