54a4f36e05e95ae581613cea53f617f581295f1a
sarthak-py <shiva02939@gmail.com>
2022-03-09 16:10:28 +0530

    bot class

--- pokemare/bot.py
+++ pokemare/bot.py
@@ -1,45 +1,44 @@
+from __future__ import annotations
+
+import os
 import json
+from datetime import datetime
 
-from disnake.flags import Intents
-from disnake.activity import Activity
-from disnake.enums import ActivityType, Status
-from disnake.ext.commands.bot import Bot, when_mentioned_or
+from discord.flags import Intents
+from discord.message import Message
+from discord.ext.commands.bot import Bot
+from discord.ext.commands.context import Context
 
-from .database import UserInfoDatabase
+from .handlers import PrefixHandler
 
 
 class PokeMare(Bot):
-    def __init__(self, token):
-        super().__init__(
-            command_prefix=when_mentioned_or("."),
-            intents=Intents.all(),
-            case_insensitive=True,
-            strip_after_prefix=True,
-            activity=Activity(type=ActivityType.listening, name=".help"),
-            status=Status.dnd,
-        )
-        self.token = token
+    def __init__(self) -> None:
+        self.prefix_database = PrefixHandler()
+        self.boot_time = datetime.now()
         self.color = 0x04356D
+        self.support_server_invite_url = os.getenv("SUPPORT_SERVER")
+        intents = Intents.all()
+        super().__init__(
+            command_prefix=self.prefix_database.get_prefix,
+            intents=intents,
+            case_insensitive=True,
+        )
         self.load_extension("jishaku")
-        self.get_cog("Jishaku").hidden = True
-        self.load_extension("pokemare.cogs.start")
-        self.load_extension("pokemare.cogs.tools")
-        self.load_extension("pokemare.cogs.general")
-        self.load_extension("pokemare.cogs.miscs")
-        self.load_extension("pokemare.cogs.help")
-        self.support_server_invite_url = "https://discord.gg/Km8WwHBSrg"
+        self.load_extensions_from("pokemare.plugins")
         with open("data/pokemons.json", "r") as file:
             self.pokemon_dict = json.load(file)
 
-    async def on_ready(self):
-        self.user_database = UserInfoDatabase("users.db", self)
-        await self.user_database.create_and_connect()
-        self.invite_url = f"https://discordapp.com/oauth2/authorize?client_id={self.user.id}&scope=bot+applications.commands&permissions=3691367512"
-        print("Bot Online", self.latency * 1000)
+    def load_extensions_from(self, path: str) -> None:
+        for file in os.listdir(path):
+            if not file.startswith("_") and file.endswith(".py"):
+                self.load_extension(path + file[:-3])
 
-    def run(self):
-        super().run(self.token)
+    async def on_ready(self) -> None:
+        self.prefix_database.connect()
 
+    async def getch_user(self, id: int) -> None:
+        return self.get_user(id) or await self.fetch_user(id)
 
-if __name__ == "__main__":
-    PokeMare = PokeMare()
+    async def set_prefix_for(self, message: Message | Context, prefix: str) -> None:
+        await self.prefix_database.set_prefix(message, prefix)
