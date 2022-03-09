c778d8d03600b6070257c5da7bbeca031f05df99
sarthak-py <shiva02939@gmail.com>
2022-03-09 17:04:45 +0530

    added change prefix command

--- pokemare/plugins/general.py
+++ pokemare/plugins/general.py
@@ -20,7 +20,37 @@ class General(Cog):
     @command(name="ping", description="Bot Latency", aliases=["latency", "pong"])
     async def _ping(self, context: Context) -> None:
         """Ping Pong"""
-        await context.reply(embed=Embed(description=f"Ping : `{round(self.bot.latency, 2)} ms`",color=self.bot.color))
+        await context.reply(
+            embed=Embed(
+                description=f"Ping : `{round(self.bot.latency, 2)} ms`",
+                color=self.bot.color,
+            )
+        )
+
+    @command(
+        name="prefix", description="Change prefix for the server", aliases=["setprefix"]
+    )
+    @has_permissions(manage_guild=True)
+    async def _set_prefix(self, context: Context, new_prefix: str) -> None:
+        """Change Prefix"""
+        if any(
+            invalid_char
+            for invalid_char in new_prefix
+            if invalid_char in ("@", "#", "\\", "`")
+        ):
+            return await context.reply(
+                embed=Embed(
+                    description="Prefix cannot have these elements : '@' , '#' , '', '`",
+                    color=self.bot.color,
+                )
+            )
+        await self.bot.prefix_database.set_prefix(context, new_prefix)
+        await context.send(
+            embed=Embed(
+                description=f"Changed prefix to `{new_prefix}`", color=self.bot.color
+            )
+        )
+
 
 def setup(bot: PokeMare) -> None:
     bot.add_cog(General(bot))
